eaa2270c315c6ad1cd141c19ce7e5df6067522f7
Lucy Smith <lucy.rose.smith@icloud.com>
2023-06-04 18:11:36 +0100

    Test

--- src/MyWeatherStation.cpp
+++ src/MyWeatherStation.cpp
@@ -6,37 +6,44 @@
 #line 1 "/Users/lucy/Desktop/project/MyWeatherStation/src/MyWeatherStation.ino"
 /*
  * Project MyWeatherStation
- * Description:
- * Author:
- * Date:
+ * Description: A level project cycle 1
+ * Author: Lucy Smith
+ * Date: 24/05/23
  */
 
-// setup() runs once, when the device is first turned on.
 #include "Adafruit_BME280.h"
+
 void setup();
 void loop();
 #line 10 "/Users/lucy/Desktop/project/MyWeatherStation/src/MyWeatherStation.ino"
 Adafruit_BME280 bme;
+
+//initialising variables for BME280 sensor
 int temp = 0;
 int humidity = 0;
 int pressure = 0;
 int altitude = 0;
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
   bme.begin();
+
+  //set variables of particle dashboard
   Particle.variable("temp", temp);
   Particle.variable("humidity", humidity);
   Particle.variable("pressure", pressure);
   Particle.variable("altitude", altitude);
-  Particle.publish("testing");
+
+  Particle.publish("Weather Station Online :)");
 }
 
-// loop() runs over and over again, as quickly as it can execute.
+
 void loop() {
-  // The core of your code will likely live here.
+
   temp = (int)bme.readTemperature();
   humidity = (int)bme.readHumidity();
   pressure = (int)bme.readPressure();
   altitude = (int)bme.readAltitude(1013.25); //sea level pressure, estimates altitude
+
+  Particle.publish("testing");
 
 }
--- src/MyWeatherStation.ino
+++ src/MyWeatherStation.ino
@@ -1,33 +1,40 @@
 /*
  * Project MyWeatherStation
- * Description:
- * Author:
- * Date:
+ * Description: A level project cycle 1
+ * Author: Lucy Smith
+ * Date: 24/05/23
  */
 
-// setup() runs once, when the device is first turned on.
 #include "Adafruit_BME280.h"
+
 Adafruit_BME280 bme;
+
+//initialising variables for BME280 sensor
 int temp = 0;
 int humidity = 0;
 int pressure = 0;
 int altitude = 0;
+
 void setup() {
-  // Put initialization like pinMode and begin functions here.
   bme.begin();
+
+  //set variables of particle dashboard
   Particle.variable("temp", temp);
   Particle.variable("humidity", humidity);
   Particle.variable("pressure", pressure);
   Particle.variable("altitude", altitude);
-  Particle.publish("testing");
+
+  Particle.publish("Weather Station Online :)");
 }
 
-// loop() runs over and over again, as quickly as it can execute.
+
 void loop() {
-  // The core of your code will likely live here.
+
   temp = (int)bme.readTemperature();
   humidity = (int)bme.readHumidity();
   pressure = (int)bme.readPressure();
   altitude = (int)bme.readAltitude(1013.25); //sea level pressure, estimates altitude
+
+  Particle.publish("testing");
 
 }
